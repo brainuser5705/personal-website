342d8dbb50dffdb311c9961c40867988af609c6c
brainuser5705 <brainuser5705@outlook.com>
2024-02-03 22:41:17 -0500

    external linking for hackathon pages, cookie to remember project type selection after clicking on a page

---
+++ _past/hackathon/inventori.md
@@ -0,0 +1,7 @@
+---
+name: Inventori
+description: practical grocery inventory management system for me and my roommate [WicHacks submission]
+time: 2023-03
+tech: js arduino
+extern: https://devpost.com/software/inventori-rcmzbk
+---
---
+++ assets/js/cookie.js
@@ -0,0 +1,26 @@
+function setCookie(type){
+    document.cookie = "type="+type;
+    updateButtons();
+}
+
+function isSelected(type){
+    return document.cookie == "type="+type;
+}
+
+function updateButtons(){
+    let buttons = document.getElementsByClassName("btn");
+    for (let button of buttons){
+        let type = button.getAttribute("aria-controls");
+        let accordionItem = document.getElementById(type);
+        let selected = isSelected(type);
+        button.setAttribute("aria-expanded", selected);
+        if (selected){
+            accordionItem.classList.add("show");
+            button.classList.remove("collapsed");
+        }else{
+            accordionItem.classList.remove("show");
+            button.classList.add("collapsed");
+        }
+        
+    }
+}
